a152a6f5cf52f860209a8d48063511baaf8ec30a
Sree-Varshitha2 <sreevarshitha04@gmail.com>
2026-04-14 21:42:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,30 +1,20 @@
 const WebSocket = require("ws");
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: PORT });
 
-let clients = [];
-
 wss.on("connection", (ws) => {
-  console.log("New client connected");
+  console.log("Client connected");
 
-  clients.push(ws);
-
-  ws.on("message", (msg) => {
-    const data = JSON.parse(msg);
-
-    // Broadcast message to all users
-    clients.forEach((client) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(data));
+        client.send(message.toString());
       }
     });
   });
 
-  ws.on("close", () => {
-    clients = clients.filter((c) => c !== ws);
-    console.log("Client disconnected");
-  });
+  ws.on("close", () => console.log("Client disconnected"));
 });
 
-console.log("Server running on port " + PORT);
+console.log("Server running on port", PORT);
